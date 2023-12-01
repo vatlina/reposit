98c177ca7f7ca21d557d48a361a193b8c73e22ec
vatlin <vatlina@gmail.com>
2023-12-01 09:07:33 +0100

    ДЗ 16 от 30.11.23

---
+++ src/jhjghg.java
@@ -0,0 +1,22 @@
+/*1. Написать метод, принимающий строку и возвращающий целое число, равное двойной длине строки
+        Пример: Hello -> 10*/
+
+public class DoubleStringLength {
+    public static void main(String[] args) {
+
+        String inputString = "Hello";
+        int result = getDoubleStringLength(inputString);
+
+        System.out.println("Двойная длина строки: " + result);
+    }
+
+        public static int getDoubleStringLength(String input) {
+
+        if (input == null) {
+            System.out.println("Строка не инициализирована.");
+            return -1;
+        }
+
+        return input.length() * 2;
+    }
+}
