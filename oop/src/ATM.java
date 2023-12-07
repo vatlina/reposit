6f09f525016aff3f63fa0b5bd905051a85af8293
vatlin <vatlina@gmail.com>
2023-12-07 16:40:43 +0100

    Консультация 07.12.23

---
+++ oop/src/ATM.java
@@ -0,0 +1,18 @@
+public class ATM {
+    public static void main(String[] args) {
+        int amount = 106;//сумма
+        int[] result = withdraw(amount);
+        System.out.println("Минимальное количество банкнот для суммы " + amount + ":");
+        System.out.println("10 долларов: " + result[0]);
+        System.out.println("5 долларов: " + result[1]);
+        System.out.println("1 доллар: " + result[2]);
+    }
+
+    public static int[] withdraw(int amount) {
+        int tens = amount / 10;
+        int fives = (amount % 10) / 5;
+        int ones = (amount % 10) % 5;
+
+        return new int[]{tens, fives, ones};
+    }
+}
