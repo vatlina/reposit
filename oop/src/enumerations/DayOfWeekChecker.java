9c1af0741d87f4d14ea27fdfa8d2f5f671b0b2bb
vatlin <vatlina@gmail.com>
2023-12-20 20:13:22 +0100

    ДЗ 30 от 20.12.23

---
+++ oop/src/enumerations/DayOfWeekChecker.java
@@ -0,0 +1,27 @@
+package enumerations;
+
+import java.util.Scanner;
+public class DayOfWeekChecker {
+        enum DayOfWeek {
+        MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
+    }
+        static boolean isWeekend(DayOfWeek day) {
+        return day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите день недели: ");
+        String userInput = scanner.nextLine().toUpperCase();
+
+        try {
+            DayOfWeek userDay = DayOfWeek.valueOf(userInput);
+            boolean isWeekend = isWeekend(userDay);
+            System.out.println("День недели " + userDay + " выходной? " + isWeekend);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Некорректный ввод. Пожалуйста, введите день недели корректно.");
+        } finally {
+            scanner.close();
+        }
+    }
+}
