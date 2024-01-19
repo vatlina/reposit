4bbb122c52da40cbc8053841d5c2b2db672037e9
vatlin <vatlina@gmail.com>
2024-01-19 09:55:13 +0100

    ДЗ 41 от 18.01.24

--- oop/src/Map/NameCounter.java
+++ oop/src/Map/NameCounter.java
@@ -1,29 +1,26 @@
 package Map;
-
 /*Задача на мапы: Дан список имен, в котором некоторые имена повторяются. Написать метод, который по имени вернет
 количество вхождений этого имени в список, т.е. метод принимает лист и имя и возвращает число, соответствующее
 количеству раз , которое это имя встречается в листе.*/
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class NameCounter {
-
     public static int countOccurrences(List<String> names, String targetName) {
+            Map<String, Integer> nameOccurrences = new HashMap<>();
 
-        Map<String, Integer> occurrencesMap = new HashMap<>();
         for (String name : names) {
-            occurrencesMap.put(name, occurrencesMap.getOrDefault(name, 0) + 1);
+            String normalized = name.toLowerCase();
+            nameOccurrences.put(normalized, nameOccurrences.getOrDefault(normalized, 0) + 1);
         }
-        return occurrencesMap.getOrDefault(targetName, 0);
+        String normalizedTarget = targetName.toLowerCase();
+        return nameOccurrences.getOrDefault(normalizedTarget, 0);
     }
     public static void main(String[] args) {
-        List<String> namesList = List.of("Иван", "Марья", "Семен", "иван", "Петр", "ИВАН", "Анна", "Марья", "Иван");
-
+        List<String> names = List.of("Иван", "Марья", "Семен", "иван", "Петр", "ИВАН", "Анна", "Марья", "Иван");
         String targetName = "Иван";
-        int occurrences = countOccurrences(namesList, targetName);
-        System.out.println("Число повторов имени " + targetName + ": " + occurrences);
+        int occurrences = countOccurrences(names, targetName);
+        System.out.println("Occurrences of " + targetName + ": " + occurrences);
     }
 }
